175e25c5bd499f47abf1095a42cf71ce7ff8ca85
SOUTaccount <okmda@yandex.ru>
2021-11-13 10:43:01 +0300

    -update timetable -update StudentProfileActivity

---
+++ app/src/main/java/com/example/repetitorhelper/RepetitorProfile.java
@@ -0,0 +1,14 @@
+package com.example.repetitorhelper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RepetitorProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repetitor_profile);
+    }
+}
